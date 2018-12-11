37bc7bd1bc5f0253365bda10f3b26fc418fd31ad
Christian Levin <christian.johansson@gmail.com>
2018-12-11 23:06:35 +0100

    Add project files.

---
+++ Adventcalendar11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventcalendar11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int frequency = 0;
+            var fileToCalculate = File.ReadAllLines("c:/Input.txt");
+            foreach (var changeInfrequency in fileToCalculate)
+            {
+                frequency = frequency + Convert.ToInt32(changeInfrequency);
+            }
+            Console.WriteLine(frequency);
+            Console.ReadKey();
+        }
+    }
+}
